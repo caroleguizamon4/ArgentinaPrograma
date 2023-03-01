991513d5023336ef87584536ba24f8f172b858ff
Carolina Leguizamon <caroleguizamon5@hotmail.com>
2023-03-01 00:59:19 -0300

    terminando mini funcion java

---
+++ script.js
@@ -0,0 +1,4 @@
+function ventanaSecundaria (URL){ 
+   window.open(URL,"ventana1","scrollbars=NO") 
+} 
+ventanaSecundaria("https://docs.google.com/forms/d/e/1FAIpQLSfZNAS3nRzks4978EoXTwKayDRKnJdEMK31oHj5hcTQuMASFA/viewform?usp=sf_link"); 
